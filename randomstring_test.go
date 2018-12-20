e5e8317e5d6788a83a64a2ee260a0846e42b1fd2
Alexander F. Rødseth <rodseth@gmail.com>
2018-12-20 11:30:26 +0100

    Initial commit

---
+++ randomstring_test.go
@@ -0,0 +1,22 @@
+package randomstring
+
+import (
+	"fmt"
+	"testing"
+)
+
+func init() {
+	Seed()
+}
+
+func TestString(t *testing.T) {
+	fmt.Printf("%x\n", String(10))
+}
+
+func TestHumanFriendlyString(t *testing.T) {
+	fmt.Printf("%s\n", HumanFriendlyString(20))
+}
+
+func TestCookieFriendlyString(t *testing.T) {
+	fmt.Printf("%s\n", CookieFriendlyString(20))
+}
